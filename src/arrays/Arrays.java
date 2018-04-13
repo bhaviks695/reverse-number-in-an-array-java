e292bbb42afb3729399d9bfbdb984520a10e3d17
Bhavik Shah <bhaviks695@gmail.com>
2018-04-13 05:30:50 -0700

    Add files via upload

---
+++ src/arrays/Arrays.java
@@ -0,0 +1,15 @@
+package arrays;
+public class Arrays {
+    
+    public static void main(String[] args) {
+       
+ int [] intArray = {1,2,3,4,5};
+ 
+ //code to reverse the array!
+ for(int i=intArray.length-1;i>=0;i--)
+ {
+   System.out.println(intArray[i]);
+ }
+      
+    }
+}
